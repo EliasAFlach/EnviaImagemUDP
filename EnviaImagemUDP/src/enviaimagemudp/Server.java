3dae7ff54e9b49469bc612be1ded7a55ad016301
Magaiver <magaiwer@hotmail.com.br>
2021-05-22 17:53:04 -0300

    Envio manual da imagem

--- EnviaImagemUDP/src/enviaimagemudp/Server.java
+++ EnviaImagemUDP/src/enviaimagemudp/Server.java
@@ -1,53 +1,90 @@
 package enviaimagemudp;
 
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.net.ServerSocket;
+import java.io.InputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.Socket;
-import java.net.SocketTimeoutException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.imageio.ImageIO;
-import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class Server extends Thread {
 
-    private ServerSocket serverSocket;
+    private static int BUFFER_SIZE = 65507;
+    private DatagramSocket datagramSocket;
     private BufferedImage img;
     Socket server;
     JFrame frame = new JFrame();
     JLabel label = new JLabel();
 
     public Server(int port) throws IOException, SQLException, ClassNotFoundException, Exception {
-        serverSocket = new ServerSocket(port);
+        datagramSocket = new DatagramSocket(port);
         frame.add(label);
+        frame.setSize(500, 500);
+        frame.setLocationRelativeTo(frame);
+        frame.setDefaultCloseOperation(3);
+        frame.setTitle("Servidor");
         frame.setVisible(true);
     }
 
     public void run() {
+        List<Byte> packets = new ArrayList<Byte>();
+        int count = 0;
+
         while (true) {
             try {
-                server = serverSocket.accept();
-                img = ImageIO.read(ImageIO.createImageInputStream(server.getInputStream()));
-                label.setIcon(new ImageIcon(img));
-                frame.pack();
-            } catch (SocketTimeoutException st) {
-                System.out.println("Socket timed out!");
-                break;
-            } catch (IOException e) {
-                e.printStackTrace();
-                break;
+                InputStream input;
+                byte[] data = new byte[BUFFER_SIZE];
+                
+                DatagramPacket packet = new DatagramPacket(data, data.length);
+                datagramSocket.receive(packet);
+                count = count + 1;
+                byte[] buffer = packet.getData();
+
+                System.out.println("Pacote n° " + count + " de tamanho " + buffer.length);
+
+                for (int i = 0; i < buffer.length; i++) {
+                    packets.add(buffer[i]);
+                }
+
+               // if (count == 24) {
+                    byte[] newBuffer = new byte[packets.size()];
+
+                    for (int i = 0; i < packets.size(); i++) {
+                        newBuffer[i] = packets.get(i);
+                    }
+
+                    input = new ByteArrayInputStream(newBuffer);
+                    img = ImageIO.read(ImageIO.createImageInputStream(input));
+                    label.setIcon(new ImageIcon(img));
+                    frame.pack();
+                  
+                    Thread.sleep(10);
+                    
+                    
+                //}
+
             } catch (Exception ex) {
                 System.out.println(ex);
             }
         }
     }
 
+    public void receiveFile() {
+
+    }
+
     public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException, Exception {
-        Thread t = new Server(6066);
+        Thread t = new Server(7788);
         t.start();
+
     }
 }
